8403b6ef2f57e996a509dcf67c29a4a6aa8a4046
Guilherme Neves <xxwhitexx.acc@gmail.com>
2025-12-24 02:22:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createCanvas, loadImage } = require("canvas");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const MAP_SIZE = 8192;
 
